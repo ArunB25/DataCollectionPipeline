d944695b20e40694d99f6c3d02934562c85cc6d9
Arun <arun.jb25@hotmail.co.uk>
2022-09-09 16:45:49 +0100

    zoopla web scraper complete, searches through 4 pages collecting all the property links, the collects data from each property

--- zoopla_scraper.py
+++ zoopla_scraper.py
@@ -6,9 +6,10 @@ class scraper:
 
     def __init__(self):
         """
-        initialises the properties dictionary
+        initialises the properties links and dictionary and page links
         """
         self.property_links = []
+        self.page_links = []
         self.dict_properties = {'Price': [], 'Address': [], 'Bedrooms': [], 'Description': []}
     
     def load_and_accept_cookies(self) -> webdriver.Chrome:
@@ -58,39 +59,61 @@ class scraper:
         time.sleep(1)
 
     def next_page(self):
-
+        """
+        Gets link from the next page button
+        """    
         pagination = self.driver.find_element(by=By.XPATH, value='//div[@data-testid="pagination"]') # XPath corresponding to the pagination section
-        pagination = pagination.find_element(by=By.CLASS_NAME, value = 'e1x1tr228 css-1gny8z8-PaginationContainer-Pagination eaoxhri0')
-        next_page_button = pagination.find_element(by=By.XPATH, value='//ul[@class="css-qhg1xn-PaginationItemPreviousAndNext-PaginationItemNext eaoxhri2"]')
+        pagination = pagination.find_element(by=By.XPATH, value='//ul[@class="e7y3oie8 css-1gny8z8-PaginationContainer-Pagination eaoxhri0"]')
+        next_page_button = pagination.find_element(by=By.XPATH, value='//li[@class="css-qhg1xn-PaginationItemPreviousAndNext-PaginationItemNext eaoxhri2"]')
         a_tag = next_page_button.find_element(by=By.TAG_NAME, value='a')
         link = a_tag.get_attribute('href')
         self.page_links.append(link)
+        time.sleep(1)
 
     def get_property_details(self,property_link):
         """
         Takes the property URL and scrapes to find the Price, address, number of bedrooms and descripton and adds them to the dictionary of properties
         """
         self.driver.get(property_link)
-        price = self.driver.find_element(by=By.XPATH, value='//p[@data-testid="price"]').text
-        self.dict_properties['Price'].append(price)
-        address = self.driver.find_element(by=By.XPATH, value='//address[@data-testid="address-label"]').text
-        self.dict_properties['Address'].append(address)
-        rooms = self.driver.find_element(by=By.XPATH, value='//div[@class="c-PJLV c-PJLV-iiNveLf-css"]')
-        bedrooms = rooms.find_element(by=By.XPATH, value='//div[@class="c-cbuYEU c-cbuYEU-egQFzo-isAnAttribute-true c-cbuYEU-iPJLV-css"]').text
-        self.dict_properties['Bedrooms'].append(bedrooms)
-        div_tag = self.driver.find_element(by=By.XPATH, value='//div[@data-testid="truncated_text_container"]')
-        span_tag = div_tag.find_element(by=By.XPATH, value='.//span')
-        description = span_tag.text
-        self.dict_properties['Description'].append(description)
+        try:
+            price = self.driver.find_element(by=By.XPATH, value='//p[@data-testid="price"]').text
+            self.dict_properties['Price'].append(price)
+        except:
+            self.dict_properties['Price'].append("NA")
+        try:
+            address = self.driver.find_element(by=By.XPATH, value='//address[@data-testid="address-label"]').text
+            self.dict_properties['Address'].append(address)
+        except:
+            self.dict_properties['Address'].append("NA")
+        try:
+            rooms = self.driver.find_element(by=By.XPATH, value='//div[@class="c-PJLV c-PJLV-iiNveLf-css"]')
+            bedrooms = rooms.find_element(by=By.XPATH, value='//div[@class="c-cbuYEU c-cbuYEU-egQFzo-isAnAttribute-true c-cbuYEU-iPJLV-css"]').text
+            self.dict_properties['Bedrooms'].append(bedrooms)
+        except:
+            self.dict_properties['Bedrooms'].append("NA")
+        try:
+            div_tag = self.driver.find_element(by=By.XPATH, value='//div[@data-testid="truncated_text_container"]')
+            span_tag = div_tag.find_element(by=By.XPATH, value='.//span')
+            description = span_tag.text
+            self.dict_properties['Description'].append(description)
+        except:
+            self.dict_properties['Description'].append("NA")
+        
+        time.sleep(1)
 
 if __name__ == "__main__":
     
     lndn_houses = scraper()
     lndn_houses.load_and_accept_cookies()
-    lndn_houses.get_page_links()
 
-    lndn_houses.next_page()
-    # for i in (lndn_houses.property_links):
-    #     lndn_houses.get_property_details(i)
+    for page in range(1,5):
+        
+        lndn_houses.get_page_links()
+        lndn_houses.next_page()
+        lndn_houses.driver.get(lndn_houses.page_links[(page -1)])
 
-    # print(lndn_houses.dict_properties)
+    for i in (lndn_houses.property_links):
+        
+        lndn_houses.get_property_details(i)
+
+    print(f"There are {len(lndn_houses.dict_properties)} properties in the dictionary")
